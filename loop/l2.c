5e7bd91f6027f98803bac50f636c939ee61d772c
Vrushabh Motghare <motgharevrushabh@gmail.com>
2020-07-18 10:21:02 +0530

    condition

---
+++ loop/l2.c
@@ -0,0 +1,36 @@
+#include <stdio.h> 
+int main()
+{
+ 	int i,j,n;
+	printf("enter the value of rows:\n");
+	scanf("%d",&n);
+	for(i=1;i<=n;i++)
+	{
+		for(j=1;j<=i;j++)
+		{
+			printf("*");
+		}
+		printf("\n");
+	}
+	
+	for(i=1;i<=n;i++)
+	{
+		for(j=1;j<=i;j++)
+		{
+			printf("%d",i);
+		}
+		printf("\n");
+	}
+	printf("\n");
+	for(i=1;i<=n;i++)
+	{
+		for(j=1;j<=i;j++)
+		{
+			printf("%d",j);
+		}
+		printf("\n");
+	}
+
+
+}
+
